53c4ed0a5c35bf272b2370ec0c0049800863b411
François <francois-lolin@live.fr>
2022-06-02 15:23:06 +0200

    save

--- sources/minishell_split.c
+++ sources/minishell_split.c
@@ -10,130 +10,130 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "minishell.h"
+#include "minishell.h"
 
-// int	ft_strstr(char c, char *find)
-// {
-// 	int	i;
+int	ft_is_space(char c, char *space)
+{
+	int	i;
 
-// 	i = 0;
-// 	while (find[i])
-// 	{
-// 		if (c == find[i])
-// 			return (TRUE);
-// 		i++;
-// 	}
-// 	return (FALSE);
-// }
+	i = 0;
+	while (space[i])
+	{
+		if (c == space[i])
+			return (TRUE);
+		i++;
+	}
+	return (FALSE);
+}
 
-// int	minishell_quote(char const *str, int i)
-// {
-// 	if (str[i] == '\"')
-// 	{
-// 		i++;
-// 		while (str[i] != '\"')
-// 		{
-// 			if (str[i] == '\\')
-// 				i++;
-// 			i++;
-// 		}
-// 	}
-// 	if (str[i] == '\'')
-// 	{
-// 		i++;
-// 		if (str[i] != '\'')
-// 		{
-// 			if (str[i] == '\\')
-// 				i++;
-// 			i++;
-// 		}
-// 	}
-// 	return (i);
-// }
+int	minishell_quote(char const *str, int i)
+{
+	if (str[i] == '\"')
+	{
+		i++;
+		while (str[i] != '\"')
+		{
+			if (str[i] == '\\')
+				i++;
+			i++;
+		}
+	}
+	if (str[i] == '\'')
+	{
+		i++;
+		if (str[i] != '\'')
+		{
+			if (str[i] == '\\')
+				i++;
+			i++;
+		}
+	}
+	return (i);
+}
 
-// int	minishell_count(char const *str, char *space)
-// {
-// 	int	i;
-// 	int	j;
-// 	int	res;
+int	minishell_count(char const *str, char *space)
+{
+	int	i;
+	int	j;
+	int	res;
 
-// 	i = 0;
-// 	j = FALSE;
-// 	res = 0;
-// 	while (str[i])
-// 	{
-// 		i = minishell_quote(str, i);
-// 		if (!j && !ft_strstr(str[i], space))
-// 			j = TRUE;
-// 		if (j && ft_strstr(str[i], space))
-// 		{
-// 			j = FALSE;
-// 			res++;
-// 		}
-// 		i++;
-// 	}
-// 	return (res);
-// }
+	i = 0;
+	j = ft_is_space(str[i], space);
+	res = 1;
+	while (str[i])
+	{
+		i = minishell_quote(str, i);
+		if (!j && ft_is_space(str[i], space))
+		{
+			j = TRUE;
+			res++;
+		}
+		if (j && !ft_is_space(str[i], space))
+			j = FALSE;
+		i++;
+	}
+	return (res);
+}
 
-// int	ft_split_len(char const *s, char c, int i)
-// {
-// 	int	j;
+int	minishell_len(char const *str, char *space, int i)
+{
+	int	j;
 
-// 	j = 0;
-// 	while (s[i])
-// 	{
-// 		if (s[i] == c)
-// 			return (j);
-// 		i++;
-// 		j++;
-// 	}
-// 	return (j);
-// }
+	j = 0;
+	while (str[i])
+	{
+		if (ft_is_space(str[i], space))
+			return (j);
+		i++;
+		j++;
+	}
+	return (j);
+}
 
-// char	*ft_split_input(char const *s, char c, int i)
-// {
-// 	int		j;
-// 	char	*res;
+char	*minishell_input(char const *str, char *space, int i)
+{
+	int		j;
+	char	*res;
 
-// 	res = malloc(sizeof(char) * (ft_split_len(s, c, i) + 1));
-// 	if (!res)
-// 		return (NULL);
-// 	j = 0;
-// 	while (s[i] && s[i] != c)
-// 	{
-// 		res[j] = s[i];
-// 		i++;
-// 		j++;
-// 	}
-// 	res[j] = '\0';
-// 	return (res);
-// }
+	res = malloc(sizeof(char) * (minishell_len(str, space, i) + 1));
+	if (!res)
+		return (NULL);
+	j = 0;
+	while (str[i] && !ft_is_space(str[i], space))
+	{
+		res[j] = str[i];
+		i++;
+		j++;
+	}
+	res[j] = '\0';
+	return (res);
+}
 
-// char	**minishell_split(char const *str, char *space)
-// {
-// 	int		i;
-// 	int		j;
-// 	char	**res;
+char	**minishell_split(char const *str, char *space)
+{
+	int		i;
+	int		j;
+	char	**res;
 
-// 	res = malloc(sizeof(char *) * (minishell_count(str, space) + 1));
-// 	if (!res)
-// 		return (NULL);
-// 	j = 0;
-// 	i = 0;
-// 	while (str[i])
-// 	{
-// 		while (s[i] == c)
-// 			i++;
-// 		if (s[i])
-// 			res[j] = ft_split_input(s, c, i);
-// 		else
-// 			break ;
-// 		if (!res[j])
-// 			return (ft_free_split(res));
-// 		while (s[i] && s[i] != c)
-// 			i++;
-// 		j++;
-// 	}
-// 	res[j] = NULL;
-// 	return (res);
-// }
+	res = malloc(sizeof(char *) * minishell_count(str, space));
+	if (!res)
+		return (NULL);
+	j = 0;
+	i = 0;
+	while (str[i])
+	{
+		while (ft_is_space(str[i], space))
+			i++;
+		if (str[i])
+			res[j] = minishell_input(str, space, i);
+		else
+			break ;
+		if (!res[j])
+			return (ft_split_free(res));
+		while (str[i] && !ft_is_space(str[i], space))
+			i++;
+		j++;
+	}
+	res[j] = NULL;
+	return (res);
+}
