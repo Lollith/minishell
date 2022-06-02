4b7b79ebb17d52a8fa8337dbd068d12c1158788e
Adeline GOUET <agouet@e1r11p22.42.fr>
2022-06-02 12:35:16 +0200

    operator.c clean

--- sources/ft_operator.c
+++ sources/ft_operator.c
@@ -6,83 +6,72 @@
 /*   By: agouet <agouet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 10:15:29 by agouet            #+#    #+#             */
-/*   Updated: 2022/06/02 10:06:54 by agouet           ###   ########.fr       */
+/*   Updated: 2022/06/02 12:34:19 by agouet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-/*
-void	monitoring_line(t_list *l_token, char **envp)
-{
-	if(l_token->next)
 
-		//if next_->content ("&&")
-		// if (||)
-		// if >
-//}
-*/
-int	ft_eperluet(t_list *l_token, char **envp)
+int	monitoring_line(t_list *l_token, char **envp)
 {
 	char	**new_token_exec;
 
 	new_token_exec = create_token_exec(l_token->content);
-	if (l_token->next && ft_strncmp(l_token->next->content, "&&", 2) == 0)
+	if (l_token->next)
+	{
+		if (ft_strncmp(l_token->next->content, "&&", 2) == 0)
+			ft_eperluet(l_token, new_token_exec, envp);
+		else if (ft_strncmp(l_token->next->content, "||", 2) == 0)
+			ft_ou(l_token, new_token_exec, envp);
+		else if (ft_strncmp(l_token->next->content, ">", 1) == 0
+			|| ft_strncmp(l_token->next->content, ">|", 2) == 0)
+			ft_redir_out(l_token, new_token_exec, envp);
+	}
+	else
 	{
 		if (ft_exec(envp, l_token->content, new_token_exec) == 0)
 			return (FAILURE);
-		else
-			ft_eperluet(l_token->next->next, envp);
 	}
-	else if (!l_token->next)
-		ft_exec(envp, l_token->content, new_token_exec);
-	return (FAILURE);
+	return (SUCCESS);
 }
 
-// fct non operationnelle
-// tester si 1er commande fail => doit lancer 2 eme commande
-int	ft_ou(t_list *l_token, char **envp)
+int	ft_eperluet(t_list *l_token, char **new_token_exec, char **envp)
 {
-	char	**new_token_exec;
-
-	new_token_exec = create_token_exec(l_token->content);
-	if (l_token->next && ft_strncmp(l_token->next->content, "||", 2) == 0)
-	{
-		if (ft_exec(envp, l_token->content, new_token_exec) == 1)
-			return (SUCCESS);
-		else
-			ft_eperluet(l_token->next->next, envp);
-	}
-	return (FAILURE);
+	if (ft_exec(envp, l_token->content, new_token_exec) == 0)
+		return (FAILURE);
+	else
+		monitoring_line(l_token->next->next, envp);
+	return (SUCCESS);
 }
 
-int ft_redir_out(t_list *l_token, char **envp)
+int	ft_ou(t_list *l_token, char **new_token_exec, char **envp)
 {
-	int 	fd;
-	char	**new_token_exec;
-	int 	fd_tmp;
+	if (ft_exec(envp, l_token->content, new_token_exec) == 1)
+		return (SUCCESS);
+	else
+		monitoring_line(l_token->next->next, envp);
+	return (SUCCESS);
+}
 
+int	ft_redir_out(t_list *l_token, char **new_token_exec, char **envp)
+{
+	int		fd;
+	int		fd_tmp;
+	char	*cmd;
+
+	cmd = l_token->next->next->content;
 	fd_tmp = dup(STDOUT_FILENO);
-	new_token_exec = create_token_exec(l_token->content);
-	if (l_token->next && ((ft_strncmp(l_token->next->content, ">", 1) == 0)
-		|| ft_strncmp(l_token->next->content, ">|", 2) == 0))
-	{
-		fd = open (l_token->next->next->content, O_WRONLY | O_TRUNC | O_CREAT, 0666);
-		if (fd < 0)
-			return (msg_perror("open "));
-		if (dup2(fd, STDOUT_FILENO) == -1)
-			return (msg_perror("dup2 "));
-		if (ft_exec(envp, l_token->content, new_token_exec) == 0)
-			return (FAILURE);
-		if (close(fd) < 0)
-			return (msg_perror("fd "));
-		else
-			ft_redir_out(l_token->next->next,envp);
-		dup2(fd_tmp, STDOUT_FILENO);
-	}
-	else if(!l_token->next)
-	{
-		if (ft_exec(envp, l_token->content, new_token_exec) == 0)
-			return (FAILURE);
-	}
-	return (FAILURE);
+	fd = open (cmd, O_WRONLY | O_TRUNC | O_CREAT, 0666);
+	if (fd < 0)
+		return (msg_perror("open "));
+	if (dup2(fd, STDOUT_FILENO) == -1)
+		return (msg_perror("dup2 "));
+	if (ft_exec(envp, l_token->content, new_token_exec) == 0)
+		return (FAILURE);
+	if (close(fd) < 0)
+		return (msg_perror("fd "));
+	dup2(fd_tmp, STDOUT_FILENO);
+	if (l_token->next->next->next)
+		monitoring_line(l_token->next->next->next, envp);
+	return (SUCCESS);
 }
