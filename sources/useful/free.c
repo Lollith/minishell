5b76fb5da7e135e00c6f91873cb581a902c5f122
Adeline GOUET <agouet@e1r4p4.clusters.42paris.fr>
2022-08-12 14:04:39 +0200

    norminette

--- sources/useful/free.c
+++ sources/useful/free.c
@@ -6,23 +6,23 @@
 /*   By: agouet <agouet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 10:47:34 by agouet            #+#    #+#             */
-/*   Updated: 2022/08/12 12:09:06 by agouet           ###   ########.fr       */
+/*   Updated: 2022/08/12 14:03:53 by agouet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	free_null(void *ptr)
+void	free_null(void **ptr)
 {
-	free(ptr);
-	ptr = NULL;
+	free(*ptr);
+	*ptr = NULL;
 }
 
 void	free_content(t_pipe *pipex)
 {
-	if ((ft_strncmp(pipex->l_start->content, "<", 1) == 0))
+	if (ft_strncmp(pipex->l_start->content, "<", 1) == 0)
 	{
-		free_null(pipex->l_start->content);
-		free_null(pipex->l_start);
+		free_null((void **)&pipex->l_start->content);
+		free_null((void **)&pipex->l_start);
 	}
 }
