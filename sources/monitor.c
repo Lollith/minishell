09f4dc629a64b4178affcd8f2d26ce91611bf1d7
Adeline GOUET <agouet@e1r9p18.clusters.42paris.fr>
2022-08-17 14:02:44 +0200

    rien

--- sources/monitor.c
+++ sources/monitor.c
@@ -6,7 +6,7 @@
 /*   By: agouet <agouet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 14:11:16 by agouet            #+#    #+#             */
-/*   Updated: 2022/08/17 11:49:21 by agouet           ###   ########.fr       */
+/*   Updated: 2022/08/17 13:52:28 by agouet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,9 +89,9 @@ int	monitoring(t_list *start, t_list *l_token, char ***envp, t_pipe *pipex)
 	char	**args_exec;
 
 	args_exec = ft_is_arg(l_token);
-	pipex->l_start = start;
 	if (args_exec == NULL)
 		return (FAILURE);
+	pipex->l_start = start;
 	if (l_token->next)
 	{
 		if (check_op(l_token, args_exec, envp, pipex) == 0)
