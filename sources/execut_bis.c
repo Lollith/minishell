454a76fee1af32c7085fdecdd14aac40a415ad89
Adeline GOUET <agouet@e1r9p18.clusters.42paris.fr>
2022-08-19 10:05:07 +0200

    normi

--- sources/execut_bis.c
+++ sources/execut_bis.c
@@ -6,7 +6,7 @@
 /*   By: agouet <agouet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 12:25:11 by frrusso           #+#    #+#             */
-/*   Updated: 2022/08/19 08:19:44 by agouet           ###   ########.fr       */
+/*   Updated: 2022/08/19 09:57:31 by agouet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,22 @@ void	ft_child_free2(char ***token, char ***envp, t_list *l_token, int ret)
 	ft_split_free(*token);
 	ft_split_free(*envp);
 	exit(ret);
+}
+
+void	fd_monitor(t_pipe *pipex)
+{
+	if ((pipex->ctrl == 0 || pipex->ctrl == 1) && pipex->pipefd[0])
+		ft_link_fd(pipex->pipefd[0], pipex->pipefd[1], STDOUT_FILENO);
+	if (pipex->pipefd[0] && pipex->ctrl == -1)
+		ft_link_fd(pipex->pipefd[1], pipex->pipefd[0], STDIN_FILENO);
+}
+
+int	ft_link_fd(int pipefd0, int pipefd1, int std)
+{
+	if (pipefd0 > 0)
+		close(pipefd0);
+	dup2(pipefd1, std);
+	if (pipefd1 > -1)
+		close(pipefd1);
+	return (SUCCESS);
 }
